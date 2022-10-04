16dd434f17fda54519c03fa4ce31bad231e40e87
Camilo Matteo <camilomatteo@iMac-de-Camilo.local>
2022-10-04 17:55:14 -0300

    :sparkles: Items added

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { Image, View, StyleSheet } from 'react-native'
 import StyledText from './StyledText'
+import RepositoryStats from './RepoStats'
+import theme from '../theme'
+
+const RepoItemHeader = ({ ownerAvatarUrl, fullName, description, languaje }) => {
+  return (
+    <View style={{ flexDirection: 'row', paddingBottom: 2 }}>
+      <View style={{ paddingRight: 10 }}>
+        <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
+      </View>
+      <View style={{ flex: 1 }}>
+        <StyledText fontWeight={'bold'}>{fullName}</StyledText>
+        <StyledText color={'secondary'}>{description}</StyledText>
+        <StyledText style={styles.languaje} blue>
+          {languaje}
+        </StyledText>
+      </View>
+    </View>
+  )
+}
 
 const RepositoryItem = (props) => {
   return (
     <View key={props.id} style={styles.container}>
-      <StyledText fontSize={'subheading'} fontWeight={'bold'}>
-        {props.fullName}
-      </StyledText>
-      <StyledText blue>{props.description}</StyledText>
-      <StyledText small>{props.stargazersCount}</StyledText>
-      <StyledText small>{props.forksCount}</StyledText>
-      <StyledText small>{props.reviewCount}</StyledText>
-      <StyledText small>{props.ratingAverage}</StyledText>
+      <RepoItemHeader {...props} />
+      <RepositoryStats {...props} />
     </View>
   )
 }
@@ -22,6 +35,20 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingBottom: 5,
     paddingTop: 5
+  },
+  languaje: {
+    padding: 4,
+    marginVertical: 4,
+    color: theme.colors.white,
+    backgroundColor: theme.colors.primary,
+    alignSelf: 'flex-start',
+    borderRadius: 4,
+    overflow: 'hidden'
+  },
+  image: {
+    width: 48,
+    height: 48,
+    borderRadius: 4
   }
 })
 
